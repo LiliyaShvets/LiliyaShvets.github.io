544e8dd8799d7241ec5925b3ef681e87c895ef9c
LiliyaShvets <lilianashv@gmail.com>
2019-08-04 23:51:08 +0300

    Waxom
    Waxom

---
+++ Waxom/counter.js
@@ -0,0 +1,78 @@
+function scroll(val, el, timeout, step) {
+    var i = 0;
+    (function() {
+      if (i <= val) {
+        setTimeout(arguments.callee, timeout);
+        document.getElementById(el).innerHTML = i;
+        i = i + step;
+      } else {
+        document.getElementById(el).innerHTML = val;
+      }
+    })();
+  }
+  scroll(3587, 'counters_numbers_3587', 400, 180);
+
+
+
+  function scroll(val, el, timeout, step) {
+    var i = 0;
+    (function() {
+      if (i <= val) {
+        setTimeout(arguments.callee, timeout);
+        document.getElementById(el).innerHTML = i;
+        i = i + step;
+      } else {
+        document.getElementById(el).innerHTML = val;
+      }
+    })();
+  }
+  scroll(207, 'counters_numbers_207', 400, 10);
+
+
+
+  function scroll(val, el, timeout, step) {
+    var i = 0;
+    (function() {
+      if (i <= val) {
+        setTimeout(arguments.callee, timeout);
+        document.getElementById(el).innerHTML = i;
+        i = i + step;
+      } else {
+        document.getElementById(el).innerHTML = val;
+      }
+    })();
+  }
+  scroll(2500, 'counters_numbers_2500', 400, 125);
+
+
+
+
+  function scroll(val, el, timeout, step) {
+    var i = 0;
+    (function() {
+      if (i <= val) {
+        setTimeout(arguments.callee, timeout);
+        document.getElementById(el).innerHTML = i;
+        i = i + step;
+      } else {
+        document.getElementById(el).innerHTML = val;
+      }
+    })();
+  }
+  scroll(873, 'counters_numbers_873', 400, 44);
+
+
+
+  function scroll(val, el, timeout, step) {
+    var i = 0;
+    (function() {
+      if (i <= val) {
+        setTimeout(arguments.callee, timeout);
+        document.getElementById(el).innerHTML = i;
+        i = i + step;
+      } else {
+        document.getElementById(el).innerHTML = val;
+      }
+    })();
+  }
+  scroll(900, 'counters_numbers_900', 400, 45);
---
+++ Waxom/js.js
@@ -0,0 +1,63 @@
+'use strict' 
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+      animationTime = 600,
+      framesCount = 600;
+
+anchors.forEach(function(item) {
+  item.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+    
+    let scroller = setInterval(function() {
+      let scrollBy = coordY / framesCount;
+      
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
+  });
+});
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+
+
+
+
